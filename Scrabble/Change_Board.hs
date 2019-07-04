a5cddf1a4a20c76373cb250aa24973d484d3c590
guntas1808 <guntassgh@gmail.com>
2019-07-04 05:52:01 +0530

    function added to input words down the board

--- Scrabble/Change_Board.hs
+++ Scrabble/Change_Board.hs
@@ -10,7 +10,7 @@ module Change_Board (
 import Scrabble.Board
 import Data.List
 
---a***********************NEW BOARD FUNCTI0NS BELOW**************************
+--[*****************************INPUT A LETTER IN THE BOARD*****************************]
 
 --to get the board elements list before the element to be changed 
 prevBoard :: (Integer,Integer) -> [((Integer,Integer),Char)] -> [((Integer,Integer),Char)]
@@ -29,16 +29,38 @@ newBoard :: ((Integer,Integer),Char) -> [((Integer,Integer),Char)] -> [((Integer
 newBoard ((rw,clm),a) board = (prevBoard (rw,clm) board) ++ [((rw,clm),a)] ++ (nexBoard (rw,clm) board)
 
 
---a****************************INPUT WORDS IN THE BOARD ACROSS******************************
+--[*****************************INPUT WORDS ACROSS THE BOARD*****************************]
 
---concat prevBoard , input and nexBoard
-putWordAcrs :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)] -> [((Integer,Integer),Char)]
-putWordAcrs (rw,clm1,clm2) a board = (prevBoard (rw,clm1) board) ++ (mkBoardStr (rw,clm1,clm2) a) ++ (nexBoard (rw,clm2) board)
 
 --return a list of coordinates (rw,clm)
 pointListAcrs :: (Integer,Integer,Integer) -> [(Integer,Integer)]
 pointListAcrs (rw,clm1,clm2) = zip (take (toInt((clm2 - clm1) + 1)) (repeat rw)) ([clm1..clm2])
 
 --zips the coordinates list with the word
-mkBoardStr :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)]
-mkBoardStr (rw,clm1,clm2) a = zip (pointListAcrs (rw,clm1,clm2)) a
+mkBoardStrA :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)]
+mkBoardStrA (rw,clm1,clm2) a = zip (pointListAcrs (rw,clm1,clm2)) a
+
+--concat prevBoard , input and nexBoard
+putWordAcrs :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)] -> [((Integer,Integer),Char)]
+putWordAcrs (rw,clm1,clm2) a board = (prevBoard (rw,clm1) board) ++ (mkBoardStrA (rw,clm1,clm2) a) ++ (nexBoard (rw,clm2) board)
+
+
+--[*****************************INPUT WORDS DOWN THE BOARD*****************************]
+
+
+--return a list of coordinates (rw,clm)
+pointListDwn :: (Integer,Integer,Integer) -> [(Integer,Integer)]
+pointListDwn (rw1,rw2,clm) = zip ([rw1..rw2]) (take (toInt((rw2 - rw1) + 1)) (repeat clm))
+
+--zips the coordinates list with the word
+mkBoardStrD :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)]
+mkBoardStrD (rw1,rw2,clm) a = zip (pointListDwn (rw1,rw2,clm)) a
+
+--Adds letters recursively to the board
+inputBoard :: [((Integer,Integer),Char)] -> [((Integer,Integer),Char)] -> [((Integer,Integer),Char)]
+inputBoard (x:xs) board = if xs == [] then newBoard x board else inputBoard xs (newBoard x board)
+
+--add words down the board
+putWordDown :: (Integer,Integer,Integer) -> [Char] -> [((Integer,Integer),Char)] -> [((Integer,Integer),Char)]
+putWordDown (rw1,rw2,clm) a board = inputBoard (mkBoardStrD (rw1,rw2,clm) a) board
+
