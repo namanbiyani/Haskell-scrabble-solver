64f578915061214343dad3d5091ffd47a6f7b493
Divyanshu <divyu@iitk.ac.in>
2019-07-07 18:06:13 +0530

    Filter function added

--- Scrabble/Filter.hs
+++ Scrabble/Filter.hs
@@ -1,39 +1,42 @@
-module Filter
+module Filter 
 (
     module Board_new,
-    module Possible_permutations,
-    tuplePoints,
-    firstList,
-    secondList,
-    filter,
-    filterPointsList,
+    genPointsfromTuple,
+    noOfEmptyCell,
+    isValid,
+    findPossTuples,
 )
 where
 
-import Possible_permutations --findEmptyPoints, findNonEmptyPoints    
 import Board_new
-import Data.List
-import Data.Char
-import Data.Map
 
---function that will give a list of points between (a,c) and (b,d) 
-tuplePoints ((a,c),(b,d)) =  [(x,y) | x <- [a..b] , y <- [c..d]] 
+type Point = (Int,Int)
 
--- function to convert tuplePoints to (Point,Char) list . Use :
---initialboard !! (columns*x + y) gives (Point,Char) at coordinate (x,y)
+--The tuple contains two points which are the ending points
+genPointsfromTuple :: (Ord b, Ord a, Num b, Num a) => [(a, b)] -> [(a, b)]
+genPointsfromTuple [(x,y),(a,b)] = if (x == a && y == b) then [(x,y)]
+                                   
+                                   else if (x == a)
+                                            then do
+                                                    if y<b then [(x,y)] ++ genPointsfromTuple [(x,y+1),(a,b)] else [(a,b)] ++ genPointsfromTuple [(a,b+1),(x,y)]
+                                  
+                                   else
+                                         do
+                                            if x<a then [(x,y)] ++ genPointsfromTuple [(x+1,y),(a,b)] else  [(a,b)] ++ genPointsfromTuple [(a+1,b),(x,y)]
 
---This takes a list with elements (points, char) and returns a list containing points
-firstList [] = []
-firstList xs = [fst (head xs)] ++ firstList (tail xs)
+--Gives the no of empty points in the tuples
+noofEmptyCell :: 
+noOfEmptyCell [] scrabbleBoard column = 0
+noOfEmptyCell (x:xs) scrabbleBoard column = if scrabbleBoard !! (column*fst(x) + snd(x)) == '*' then (noOfEmptyCell xs scrabbleBoard column + 1) else noOfEmptyCell xs scrabbleBoard column
 
---This takes a list with elements (points, char) and returns a list containing char
-secondList [] = []
-secondList xs = [snd (head xs)] ++ firstList (tail xs)
+--This function evaluates whether the given tuple is valid or not
+isValid a count = if count > 7 then False
+                  else if count == 0 then False
+                  else if count == length a then False 
+                  else True
 
-filter tuples board = 
-
-filterPointsList pointList
-  | length (findEmptyPoints pointList) >= 7 = False
-  | (length findNonEmptyPoints) == (length pointList) = False 
-  | otherwise = True
-
+-- This function is main and should be called with the scrabble board and list of tuples of form [(a,b),(x,y)] and no of columns and it returns a list of the valid tuples by checkomg against scrabble board.
+findPossTuples scrabbleBoard [] column = []
+findPossTuples scrabbleBoard xs column = do
+                                         let ys = map (genPointsfromTuple) xs
+                                         [y| y <- ys, isValid y (noOfEmptyCell y scrabbleBoard column)]
